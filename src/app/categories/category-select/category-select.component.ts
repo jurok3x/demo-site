1f64344dbab68f0704666e3c2cf69c14b3895f37
ykotsiuba <jurok3x@gmail.com>
2022-09-09 18:21:42 +0300

    updates to deploy to heroku

--- src/app/categories/category-select/category-select.component.ts
+++ src/app/categories/category-select/category-select.component.ts
@@ -1,23 +1,21 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { Category } from '../../model/category';
 import { CategoriesService } from '../../service/categories.service';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-category-select',
   templateUrl: './category-select.component.html',
-  styleUrls: ['./category-select.component.css']
+  styleUrls: ['./category-select.component.css'],
 })
-export class CategorySelectComponent implements OnInit  {
+export class CategorySelectComponent implements OnInit {
 
   categories: Category[] = [];
   @Input() parentForm! : FormGroup;
   @Output() categoryEvent = new EventEmitter<number>();
 
   constructor(private categoryService: CategoriesService) {}
-  
 
   ngOnInit(): void {
     
